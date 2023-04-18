a575158bbd1d2721e9a5bbcef41124ba37bf14a1
Julio <julioagbarrios@gmail.com>
2023-04-18 15:30:02 -0300

    fix

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -5,7 +5,7 @@ export const Contact = () => {
       className="max-w-contentContainer mx-auto py-10 xl:py-32 flex flex-col gap-4 items-center justify-center"
     >
       <p className="font-titleFont text-lg text-textGreen font-semibold flex items-center tracking-wide">
-        What's Next
+        What´s Next
       </p>
       <h2 className="font-titleFont text-5xl font-semibold">Get In Touch</h2>
       <p className="max-w-[600px] text-center text-textDark">
